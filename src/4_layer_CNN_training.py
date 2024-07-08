0b43ae07001cf0e49cceb86c08225c2cb241d814
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-07-08 19:38:59 +0200

    Update 4_layer_CNN_training.py

--- src/4_layer_CNN_training.py
+++ src/4_layer_CNN_training.py
@@ -1,1 +1,24 @@
+# We train and plot for 30 epochs
+lr = 0.1
+optimizer = optim.Adam(model2.parameters(), lr=lr)
+loss_fn = nn.CrossEntropyLoss()
+n_epochs = 30
+# early_stopper = setup.EarlyStopper(patience=10)
+train_losses, val_losses, train_accs, val_accs, confusion_matrix = setup.run_training(model=model2, optimizer=optimizer, loss_function=loss_fn, 
+                                                                                      device=device, num_epochs=n_epochs, 
+                                                                                      train_dataloader=train_loader, val_dataloader=val_loader, 
+                                                                                      early_stopper=None, verbose=False)
 
+best_val_loss,best_val_loss_epoch = np.min(val_losses), np.argmin(val_losses)+1
+best_val_acc, best_val_acc_epoch = np.max(val_accs), np.argmax(val_accs)+1
+
+setup.plot(f'Loss vs. epoch (lr={lr})', 'Loss', train_losses, val_losses,
+          extra_pt=(best_val_loss_epoch, best_val_loss), 
+           extra_pt_label='Best Validation Loss')
+plt.tight_layout()
+print(f"Best Validation Loss (overall): {best_val_loss} @ Epoch no. {best_val_loss_epoch}")
+setup.plot(f'Accuracy vs. epoch (lr={lr})', 'Accuracy', train_accs, val_accs,
+           extra_pt=(best_val_acc_epoch, best_val_acc), 
+           extra_pt_label='Best Validation Accuracy')
+plt.tight_layout()
+print(f"Best Validation Accuracy (overall): {best_val_acc} @ Epoch no. {best_val_acc_epoch}")
